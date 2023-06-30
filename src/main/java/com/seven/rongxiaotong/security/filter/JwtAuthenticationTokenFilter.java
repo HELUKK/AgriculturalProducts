0c10145eef77f5a915db2b3cfad11fd1a7e166b4
GuluGulu <2295980270@qq.com>
2023-06-30 17:06:42 +0800

    添加了redis配置

--- src/main/java/com/seven/rongxiaotong/security/filter/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/seven/rongxiaotong/security/filter/JwtAuthenticationTokenFilter.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+public class   JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     @Resource
     private UserDetailsService userDetailsService;
     @Autowired
