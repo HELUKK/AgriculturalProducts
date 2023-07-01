8a3db5c59efa8a9801870d0a102726e0cda2214c
愤怒的西瓜君 <2412661873@qq.com>
2023-07-01 09:00:37 +0800

    权限放开方便测试,最后记得修改

--- src/main/java/com/seven/rongxiaotong/security/config/WebSecurityConfig.java
+++ src/main/java/com/seven/rongxiaotong/security/config/WebSecurityConfig.java
@@ -75,7 +75,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         "/user/add/**",
                         "/order/goods/**","/order/All/**","/order/needs/**","/order/selectById/**","/order/searchGoodsByKeys/**","/order/searchNeedsByKeys/**","/order/searchAllByKeys/**",
                         "/knowledge/**",
-                        "/paySuccessful/**","/user/register/**",
+                        "/paySuccessful/**","/**",
                         "/file/**").permitAll()
                 .anyRequest().authenticated()   // 任何请求,登录后可以访问
                 .and().addFilterAt(customAuthenticationFilter(),//JSON登陆实现，在 UsernamePasswordAuthenticationFilter，之前添加JSON格式
